667a2d5b25285e94f3fa8d9e47f08dde4c871340
Giulia Bondi Mazzoni <giulia.bondimazzoni@gmail.com>
2025-04-18 18:34:06 +0200

    Project set up

---
+++ src/main/java/com/catWithATemper/Skilly/SkillyApplication.java
@@ -0,0 +1,13 @@
+package com.catWithATemper.Skilly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catWithATemper/Skilly/SkillyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catWithATemper.Skilly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
